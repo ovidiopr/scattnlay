9e596ef2a8dd06eab65b6ccee408c7cac1a21f5e
Konstantin Ladutenko <kostyfisik@gmail.com>
2021-04-14 15:07:54 +0300

    update

--- tests/mpmath_special_functions_test_generator.py
+++ tests/mpmath_special_functions_test_generator.py
@@ -3,6 +3,16 @@ import mpmath as mp
 import mpmath_riccati_bessel as mrb
 import mpmath_input_arguments as mia
 
+class test_data:
+    def __init__(self, list_to_parse):
+        self.evaluated_data = []
+        self.comment = list_to_parse[0]
+        if self.comment[:2] != '//': raise ValueError('Not a comment')
+        self.ending = list_to_parse[-1]
+        if self.ending != '};': raise ValueError('For C++ we expect closing };')
+        self.list_to_parse = list_to_parse
+
+
 class update_special_functions_evaluations:
     def __init__(self, filename='default_out.h', complex_arguments = []):
         self.evaluated_data = []
@@ -10,8 +20,16 @@ class update_special_functions_evaluations:
         self.filename = filename
         self.complex_arguments = complex_arguments
 
-    def read_evaluated_date(self):
-        pass
+    def read_evaluated_data(self):
+        with open(self.filename, 'r') as in_file:
+            content = in_file.readlines()
+        content = [x.strip() for x in content]
+        while '' in content:
+            record_end_index = content.index('')
+            new_record = content[:record_end_index]
+            self.evaluated_data.append(test_data(new_record))
+            content = content[record_end_index+1:]
+        self.evaluated_data.append(test_data(new_record))
 
 
 def get_n_list(z, max_number_of_elements = 10):
@@ -45,7 +63,7 @@ def get_test_data_nlist(z_record, output_dps, n):
     return z_str
 
 def get_test_data(Du_test, output_dps, max_num_elements_of_n_list):
-    output_str = ('// complex(z), n, complex(D1_n(z)), abs_err_real, abs_err_imag\n'+
+    output_str = ('// complex(z), n, complex(D1(n,z)), abs_err_real, abs_err_imag\n'+
     'std::vector< std::tuple< std::complex<double>, int, std::complex<double>, double, double > >'+
     'D1_test_'+str(output_dps)+'digits = {\n')
     for z_record in Du_test:
@@ -75,14 +93,15 @@ def get_test_data(Du_test, output_dps, max_num_elements_of_n_list):
 
 
 def main ():
-    sf_evals = update_special_functions_evaluations(filename='test_spec_functions_data.h',
+    sf_evals = update_special_functions_evaluations(filename='test_spec_functions_data.hpp',
                                                     complex_arguments = mia.complex_arguments)
     sf_evals.read_evaluated_data()
+    print(sf_evals.evaluated_data[1].list_to_parse)
     output_dps = 16
     max_num_elements_of_nlist = 51
 
-    out_filename = 'test_spec_functions_data.h'
-    output_str = get_test_data(mia.complex_arguments, output_dps, max_num_elements_of_nlist)
+    # out_filename = 'test_spec_functions_data.hpp'
+    # output_str = get_test_data(mia.complex_arguments, output_dps, max_num_elements_of_nlist)
     # with open(out_filename, 'w') as out_file:
     #     out_file.write(output_str)
 
