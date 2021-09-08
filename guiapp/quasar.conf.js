58facee3a102eded446860a04b9583d22e868931
Konstantin Ladutenko <kostyfisik@gmail.com>
2021-09-08 14:25:01 +0300

    initial wasm import

--- guiapp/quasar.conf.js
+++ guiapp/quasar.conf.js
@@ -52,6 +52,13 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
+      extendWebpack (cfg, { isServer, isClient }) {
+        cfg.module.rules.push({
+          enforce: 'pre',
+          test: /\.wasm$/,
+          loader: 'wasm-loader'
+        })
+      },
       vueRouterMode: 'hash', // available values: 'hash', 'history'
 
       // transpile: false,
