18fa6cab570b786896294b4fc304a91e04c028db
Konstantin Ladutenko <kostyfisik@gmail.com>
2024-02-21 19:12:26 +0300

    [chore] fix deply script

--- guiapp/deploy.sh
+++ guiapp/deploy.sh
@@ -2,11 +2,11 @@
 # build, copy build files, and copy (manually) content of index.html to https://physics.itmo.ru/ru/node/2611/edit
 
 quasar build
-cp dist/spa/nmiejs.wasm dist/spa/js/nmiejs.wasm
+# cp dist/spa/nmiejs.wasm dist/spa/js/nmiejs.wasm
 
 cd ~/coding/kostyfisik.github.io
 git reset --hard c66101abdd5db0a4ff5ef69c3328cdfdbadaa398
-# cp -r ../scattnlay/guiapp/dist/spa/* ./
+cp -r ../scattnlay/guiapp/dist/spa/* ./
 git add *
 git add  */*
 git commit -am 'update'
