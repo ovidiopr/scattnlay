8dced0201a25a65b1699b933dc666bd885032fb6
Konstantin Ladutenko <kostyfisik@gmail.com>
2025-12-25 22:43:00 +0300

    f

--- guiapp/src/store/simulation-setup/actions.ts
+++ guiapp/src/store/simulation-setup/actions.ts
@@ -20,27 +20,35 @@ const actions: ActionTree<simulationSetupStateInterface, StateInterface> = {
     };
     commit('setNmies', nmies);
 
-    // // Test nmiejs if working
-    // if (nmies.spectrum && !nmies.spectrum.isNmieRunning) {
-    //   commit('markNmieAsStarted')
-    //   nmies.spectrum.ClearTarget()
-    //   const R = 100.0
-    //   const reN = 4.0
-    //   const imN = 0.01
-    //   nmies.spectrum.AddTargetLayerReIm(R, reN, imN)
-    //   nmies.spectrum.SetModeNmaxAndType(-1, -1)
-    //   const WL = 800
-    //   nmies.spectrum.SetWavelength(WL)
-    //   nmies.spectrum.RunMieCalculation()
-    //   console.log(nmies.spectrum.GetQsca())
-    //   // outer_arc_points, radius_points, from_Rho, to_Rho,
-    //   // from_Theta, to_Theta, from_Phi, to_Phi, isIgnoreAvailableNmax
-    //   nmies.spectrum.RunFieldCalculationPolar(2, 2,
-    //       0.1, 1.5, 0, 3.1415, 0, 3.1415,
-    //       0)
-    //   console.log('Field Eabs:', nmies.spectrum.GetFieldEabs())
-    //   commit('markNmieAsFinished')
-    // }
+    // Test nmiejs if working
+    if (nmies.spectrum && !nmies.spectrum.isNmieRunning) {
+      commit('markNmieAsStarted');
+      nmies.spectrum.ClearTarget();
+      const R = 100.0;
+      const reN = 4.0;
+      const imN = 0.01;
+      nmies.spectrum.AddTargetLayerReIm(R, reN, imN);
+      nmies.spectrum.SetModeNmaxAndType(-1, -1);
+      const WL = 800;
+      nmies.spectrum.SetWavelength(WL);
+      nmies.spectrum.RunMieCalculation();
+      console.log(nmies.spectrum.GetQsca());
+      // outer_arc_points, radius_points, from_Rho, to_Rho,
+      // from_Theta, to_Theta, from_Phi, to_Phi, isIgnoreAvailableNmax
+      nmies.spectrum.RunFieldCalculationPolar(
+        2,
+        2,
+        0.1,
+        1.5,
+        0,
+        3.1415,
+        0,
+        3.1415,
+        0
+      );
+      console.log('Field Eabs:', nmies.spectrum.GetFieldEabs());
+      commit('markNmieAsFinished');
+    }
 
     $q.loading.hide();
   },
